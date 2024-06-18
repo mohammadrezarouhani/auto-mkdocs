35e221ec2eae648fd2de1225f9ded9e668faa482
guts <the_guts@outlook.com>
2024-06-18 16:00:58 +0330

    🎨 moving README, docs structure, generate setting file completed

---
+++ auto_mkdoc/constant.py
@@ -0,0 +1,55 @@
+from typing import List
+from pathlib import Path, WindowsPath
+
+PACKAGE_PATH = ""
+
+MD_PATH_LIST: List[WindowsPath] = []
+
+DOCUMENT_ROOT = Path("docs")
+NAV = []
+GRAY_LIST = [".venv", "__pycashe__", "__init__", "setup"]
+
+CONFIG = {
+    "site_name": "",
+    "site_description": "",
+    "repo_url": "",
+    "site_url": "",
+    "site_author": "",
+    "repo_name": "",
+    "copyright": "",
+    "theme": {
+        "name": "material",
+        "palette": [
+            {
+                "media": "(prefers-color-scheme: light)",
+                "scheme": "default",
+                "primary": "blue grey",
+                "accent": "indigo",
+                "toggle": {
+                    "icon": "material/lightbulb-outline",
+                    "name": "Switch to dark mode",
+                },
+            },
+            {
+                "media": "(prefers-color-scheme: dark)",
+                "scheme": "slate",
+                "primary": "blue grey",
+                "accent": "indigo",
+                "toggle": {
+                    "icon": "material/lightbulb",
+                    "name": "Switch to light mode",
+                },
+            },
+        ],
+        "features": [
+            "content.code.annotate",
+            "content.tabs.link",
+            "content.code.copy",
+            "announce.dismiss",
+            "navigation.tabs",
+            "search.suggest",
+            "search.highlight",
+        ],
+    },
+    "plugins": ["mkdocstrings", "search"],
+}
--- auto_mkdoc/generator.py
+++ auto_mkdoc/generator.py
@@ -1,60 +1,118 @@
+from multiprocessing import Value
 import os
 import pdb
+import sys
+from turtle import pd
 import yaml
-import json
 import click
-from pathlib import Path
 
-# represent the root pachage that is doc
-root = Path("docs")
-
-# iterated package module with full path
-module_structure = {}
+from pathlib import Path, WindowsPath
+from collections import OrderedDict
+from constant import *
 
 
-def iterate_folder(path: Path):
+def generate_md_files(module_path: WindowsPath):
+
+    # add docs folder as root
+    nav_url = DOCUMENT_ROOT / module_path
+    temp = list(nav_url.parts)
+    temp.pop(1)
+
+    # change the suffix to .md
+    nav_url = Path(*temp).with_suffix(".md")
+
+    # creating parent folder and target module md file
+    nav_url.parent.mkdir(parents=True, exist_ok=True)
+    nav_url.touch(exist_ok=True)
+
+    # writing the module mapper in md files handles in linux and windows
+    # change the file system path to md readble mapper
+    if "/" in str(module_path):
+        md_path = ":::" + str(module_path).replace("/", ".")
+    else:
+        md_path = ":::" + str(module_path).replace("\\", ".")
+
+    # removing the suffix from md_path
+    md_path = os.path.splitext(md_path)[0]
+
+    # write the mapper to target md file
+    with open(nav_url, mode="w") as file:
+        file.write(md_path)
+
+    return nav_url
+
+
+def iterate_package(path: WindowsPath, generated_target_path=[]) -> List[WindowsPath]:
     """iterate the given folder in current work space and form the /doc folder structure
 
     Args:
         path (Path): the given path to project directory
+        generated_target_path (List(WindowPath)): python file inside target package
+
+    Returns:
+        (List[WindowsPath]): all of the gathered python files
     """
-    for entry in path.iterdir():
-        if entry.is_dir():
-            # iterate inside if the entry is folder
-            iterate_folder(entry)
+    package_content=path.iterdir()
+    
+    for entry in package_content:
+        if entry.stem in GRAY_LIST:
+            continue
 
-        elif entry.suffix == ".py" and not '__init__' in str(entry):
-            click.echo(entry)
+        elif entry.suffix == ".py":
+            # check path not in gray list
+            res = generate_md_files(entry)
+            generated_target_path.append(res)
 
-            # changing root to /doc
-            new_path = root / entry
-            temp = list(new_path.parts)
-            temp.pop(1)
+        elif entry.is_dir():
+            # iterate over if the path is folder
+            generated_target_path = iterate_package(entry, generated_target_path)
 
-            # creating relative md file
-            new_path = Path(*temp).with_suffix(".md")
-            new_path.parent.mkdir(parents=True, exist_ok=True)
-            new_path.touch(exist_ok=True)
-
-            if "//" in str(entry):
-                md_path = str(entry).replace("//", ".")
-            else:
-                md_path = str(entry).replace("\\", ".")
-
-            md_path=os.path.splitext(md_path)[0]
-
-            with open(new_path, mode="w") as file:
-                file.write(f":::{md_path}")
+    return generated_target_path
 
 
-def create_doc_struct_file():
+def mov_README_file(path: Path):
+    """mov the README file of taget package to index.md
+
+    Args:
+        path (Path): target pckage path
+    """
+    file_path = os.path.join(path, "README.md")
+
+    if os.path.exists(file_path):
+        with open(file_path) as file:
+            content = file.read()
+
+        with open("docs/index.md", mode="w") as file:
+            file.write(content)
+
+
+def create_mkdocs_conf(site_name="", site_description="", site_author=""):
+    CONFIG["site_name"] = site_name
+    CONFIG["site_description"] = site_description
+    CONFIG["site_author"] = site_author
+
+    with open("mkdocs.yml", mode="w") as file:
+        yaml.dump(CONFIG, file, sort_keys=False, indent=4)
+
+
+def create_nav_bar():
     """creating structure.json"""
-    doc_struct = []
-    for module in module_structure:
-        doc_struct.append(dict(title="", descreption="", path=str(module)))
+    navigation = []
+    for path in MD_PATH_LIST:
+        mapper = {}
 
-    with open("structure.json", mode="w") as file:
-        json.dump(doc_struct, file, indent=4)
+        while str(path) != "docs":
+            if mapper:
+                mapper = {str(path.stem): mapper}
+            else:
+                mapper = {str(path.stem): path.name}
+
+            path = path.parent
+
+        else:
+            navigation.append(mapper)
+
+    pdb.set_trace()
 
 
 @click.command("main")
@@ -69,8 +127,17 @@ def create_doc_struct_file():
     ),
 )
 def main(path):
-    iterate_folder(path)
-    create_doc_struct_file()
+    # click.echo("Welcome To AutoMkDocs", color=True)
+    # click.prompt("please enter the site name:")
+    # click.prompt("please enter the site_description name:")
+    # click.prompt("please enter the author name:")
+
+    # iterate over the target package and gathering all python files
+    res = iterate_package(path)
+    MD_PATH_LIST.extend(res)
+    mov_README_file(path)
+    create_mkdocs_conf()
+    # create_nav_bar()
 
 
 if __name__ == "__main__":
