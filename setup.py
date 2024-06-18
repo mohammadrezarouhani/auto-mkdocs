140aa369a88b11963497e210c69b18d24349c18a
guts <the_guts@outlook.com>
2024-06-18 16:10:47 +0330

    setuptools completed

--- setup.py
+++ setup.py
@@ -1,23 +1,19 @@
 from setuptools import setup
 
 setup(
-    name="Auto MkDoc",
-    version="1.0",
-    description="auto genrating document for python packages",
-    author="Guts",
-    author_email="",
-    url="",
+    name="auto_mkdocs",
+    version="1.1.0",
+    description="auto genrating document for python projects",
+    author="mohammadreza rouhani",
+    author_email="rezarouhanitonekaboni@gmail.com",
     packages=["mkdocs", "mkdocs-material", "mkdocstrings-python", "click", "pyyaml"],
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/yourusername/mypackage",
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "Operating System :: OS Independent",
-    ],
+    url="https://github.com/mohammadrezarouhani/auto_mkdocs",
+    install_requires=open("requirements.txt").readlines(),
     entry_points={
         "console_scripts": [
-            "auto_mkdoc = generate_script:main",
+            "auto_mkdocs = generate_script:main",
         ],
     },
 )
