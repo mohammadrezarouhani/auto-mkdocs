6529a2f27f333aa10633058ca9da351fea3ebaf6
Guts <the_guts@outlook.com>
2024-06-22 16:23:15 +0330

    🐛 moudle & setup issue fixed

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     install_requires=open("requirements.txt").readlines(),
     entry_points={
         "console_scripts": [
-            "auto_mkdocs = commands",
+            "auto_mkdoc = commands:main",
         ],
     },
 )
